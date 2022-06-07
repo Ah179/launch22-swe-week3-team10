e7a9d3919d3585909399b087c5badc04355f25aa
julian donald <zentixzaneic@gmail.com>
2022-06-07 17:29:56 -0400

    restructure code

--- api/routes/books.js
+++ api/routes/books.js
@@ -3,40 +3,47 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/', async (req, res, next) => {
-	const subject = 'love';
-	let works = [];
-	await axios.get(`https://openlibrary.org/subjects/${subject}.json`)
-		.then((response) => {
-			// parse the necessary data for each work
-			response.data.works.filter((work) => work.availability.isbn !== null)
-				.forEach((work) => {
-					works.push({ 
-						key: work.key,
-						title: work.title,
-						isbn: work.availability.isbn,
-						authors: work.authors,
-						cover: `https://covers.openlibrary.org/b/isbn/${work.availability.isbn}-L.jpg`,
+	try {
+		const subject = 'fiction';
+		let works = [];
+		await axios.get(`https://openlibrary.org/subjects/${subject}.json`)
+			.then((response) => {
+				// parse the necessary data for each work
+				response.data.works.filter((work) => work.availability.isbn)
+					.forEach((work) => {
+						works.push({ 
+							key: work.key,
+							title: work.title,
+							isbn: work.availability.isbn,
+							authors: work.authors,
+							cover: `https://covers.openlibrary.org/b/isbn/${work.availability.isbn}-L.jpg`,
+						});
 					});
-				});
-		})
-		.catch((error) => res.status(500).send({'message': error}));
+			});
+		console.log(works);
 
-	// get the description of each work
-	// https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
-	const descriptions = [];
-	for (const work of works) {
-		let description;
-		await axios.get(`https://openlibrary.org/${work.key}`)
-			.then((response) => description = response.data.description)
-			.catch((error) => res.status(500).send({'message': error}))
-		descriptions.push(description);
+		// get the description of each work
+		// https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
+		const bookInfo = [];
+		for (const work of works) {
+			await axios.get(`https://openlibrary.org/${work.key}`)
+				.then((response) => {
+					bookInfo.push({
+						description: response.data.description,
+						numberOfPages: response.data.number_of_pages,
+					})
+				});
+		}
+		works = works.map((work, id) => {
+			return {
+				...work,
+				...bookInfo[id],
+			}});
+		res.status(200).send({works: works});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({'message': error});
 	}
-	works = works.map((work, id) => {
-		return {
-			...work,
-			description: descriptions[id],
-		}});
-	res.status(200).send({works: works});
 })
 
 module.exports = router;
