b03c52ad99e33df3ba1334b3987c3d9488c7ddcc
julian donald <zentixzaneic@gmail.com>
2022-06-07 14:18:03 -0400

    design an error page

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,7 +1,32 @@
+import { Typography, Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { teal } from '@mui/material/colors';
+import { Link } from 'react-router-dom';
+
+const theme = createTheme({
+	palette: {
+		navbutton: {
+			main: teal[100],
+		}
+	}
+})
+
 const Error = props => {
 	return (
 		<>
-			ayo this page don't exist
+		 	<ThemeProvider theme={theme}>
+				<Typography variant="h4" sx={{ mt: 1 }}>You shouldn't walk and read.</Typography>
+				<Typography variant="subtitle1">Error 404 - This page doesn't exist!</Typography>
+				<Button
+					variant="contained"
+					color="navbutton"
+					component={Link}
+					to='/'
+					sx={{ m: 1 }}
+				>
+					Go to Books	
+				</Button>
+			</ThemeProvider>
 		</>
 	);
 };
