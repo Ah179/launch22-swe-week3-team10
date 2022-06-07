c034b9df3e030d065ba05273e4c8d9b12e7e3a36
julian donald <zentixzaneic@gmail.com>
2022-06-07 10:15:39 -0400

    add buttons to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,28 @@
+import { Link, useLocation } from 'react-router-dom';
+import { Button, Typography, AppBar } from '@mui/material';
+
 const Navbar = props => {
 	return (
 		<>
-			le website
+			<AppBar position="static">
+				<Typography variant="h4">Bookly</Typography>
+				<nav>
+					<Button
+						variant={useLocation().pathname==='/' ? 'contained' : 'text'}
+						component={Link}
+						to='/'
+					>
+						Books	
+					</Button>
+					<Button
+						variant={useLocation().pathname === '/cart' ? 'contained' : 'text'}
+						component={Link}
+						to='/cart'
+					>
+						Cart
+					</Button>
+				</nav>
+			</AppBar>
 		</>
 	);
 };
