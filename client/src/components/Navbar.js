08cd8de4b7bc4eea5688d4567b5b29c2f42a8d2c
julian donald <zentixzaneic@gmail.com>
2022-06-07 10:56:38 -0400

    fix centering on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,36 @@
 import { Link, useLocation } from 'react-router-dom';
-import { Button, Typography, AppBar } from '@mui/material';
+import { Button, Typography, AppBar, Grid, Toolbar } from '@mui/material';
 
 const Navbar = props => {
 	return (
 		<>
 			<AppBar position="static">
-				<Typography variant="h4">Bookly</Typography>
-				<nav>
-					<Button
-						variant={useLocation().pathname==='/' ? 'contained' : 'text'}
-						component={Link}
-						to='/'
-					>
-						Books	
-					</Button>
-					<Button
-						variant={useLocation().pathname === '/cart' ? 'contained' : 'text'}
-						component={Link}
-						to='/cart'
-					>
-						Cart
-					</Button>
-				</nav>
+				<Toolbar>
+					<Grid container spacing="2" justifyContent="center">
+						<Grid item xs="auto">
+							<Typography variant="h4" align="left">Bookly</Typography>
+						</Grid>
+						<Grid item xs={10}>
+							<nav>
+								<Button
+									variant={useLocation().pathname==='/' ? 'contained' : 'text'}
+									component={Link}
+									to='/'
+								>
+									Books	
+								</Button>
+								<Button
+									variant={useLocation().pathname === '/cart' ? 'contained' : 'text'}
+									component={Link}
+									to='/cart'
+								>
+									Cart
+								</Button>
+							</nav>
+						</Grid>
+						<Grid item xs></Grid>
+					</Grid>
+				</Toolbar>
 			</AppBar>
 		</>
 	);
