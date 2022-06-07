cf0ab5eb66c61d55c901a718fda331bb2c2158c8
julian donald <zentixzaneic@gmail.com>
2022-06-07 11:40:59 -0400

    style the navbar a bit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,37 +1,53 @@
 import { Link, useLocation } from 'react-router-dom';
 import { Button, Typography, AppBar, Grid, Toolbar } from '@mui/material';
+import { teal } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+	palette: {
+		navbar: {
+			main: teal[100]
+		},
+	}
+})
 
 const Navbar = props => {
 	return (
 		<>
-			<AppBar position="static">
-				<Toolbar>
-					<Grid container spacing="2" justifyContent="center">
-						<Grid item xs="auto">
-							<Typography variant="h4" align="left">Bookly</Typography>
+		 	<ThemeProvider theme={theme}>
+				<AppBar position="static" color="navbar">
+					<Toolbar>
+						<Grid container spacing="2" justifyContent="center">
+							<Grid item xs="auto">
+								<Typography variant="h4" align="left">Bookly</Typography>
+							</Grid>
+							<Grid item xs={10}>
+								<nav>
+									<Button
+										variant={useLocation().pathname === '/' ? 'contained' : 'text'}
+										component={Link}
+										to='/'
+										sx={{ mx: 1 }}
+										startIcon={null}
+									>
+										Books	
+									</Button>
+									<Button
+										variant={useLocation().pathname === '/cart' ? 'contained' : 'text'}
+										component={Link}
+										to='/cart'
+										sx={{ mx: 1 }}
+										startIcon={null}
+									>
+										Cart
+									</Button>
+								</nav>
+							</Grid>
+							<Grid item xs></Grid>
 						</Grid>
-						<Grid item xs={10}>
-							<nav>
-								<Button
-									variant={useLocation().pathname==='/' ? 'contained' : 'text'}
-									component={Link}
-									to='/'
-								>
-									Books	
-								</Button>
-								<Button
-									variant={useLocation().pathname === '/cart' ? 'contained' : 'text'}
-									component={Link}
-									to='/cart'
-								>
-									Cart
-								</Button>
-							</nav>
-						</Grid>
-						<Grid item xs></Grid>
-					</Grid>
-				</Toolbar>
-			</AppBar>
+					</Toolbar>
+				</AppBar>
+			</ThemeProvider>
 		</>
 	);
 };
