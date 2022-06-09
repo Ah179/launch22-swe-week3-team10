1b57b01e1191b52f19e12e849c44be7fc4cca51c
Keshav Bhateja <keshavbhateja1@gmail.com>
2022-06-09 17:28:45 -0400

    remove and add in cart

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,22 @@ function App() {
   const[productData, setProductData] = useState([])
   const[cartData, setCartData] = useState([])
 
+  const [counter, setCounter] = useState(0);
+ 
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  //decrease counter
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+
   const addToCart = (data) => {
     setCartData([...cartData, data])
   }
+  
 
   const removeFromCart = (data) => {
     let i = cartData.indexOf(data)
@@ -23,6 +36,14 @@ function App() {
     setCartData(newCart)
     console.log(newCart)
   }
+  // const handleChange = (data, d) => {
+  //   const ind = cartData.indexOf(data);
+  //   const arr = cartData;
+  //   arr[ind].amount += d;
+
+  //   if (arr[ind].amount === 0) arr[ind].amount = 1;
+  //   setCartData([...arr]);
+  // };
   
   useEffect(()=>{async function getBook()
     { const response= axios.get('/books');
@@ -36,7 +57,7 @@ function App() {
   
   return (
     <productDataContext.Provider value={productData}>
-      <cartContext.Provider value={{cartData, addToCart, removeFromCart}}>
+      <cartContext.Provider value={{cartData, setCartData, addToCart, removeFromCart, setCounter, counter, increase, decrease}}>
     <div className="App">
       <Navbar />
       <Outlet />
--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,41 +1,94 @@
 import React from 'react';
 import "./Cart.css";
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import {cartContext} from '../App';
-import { Button, Typography } from '@mui/material';
+import { Button} from '@mui/material';
 
 
 function Cart() {
-  const {cartData, addToCart, removeFromCart} = useContext(cartContext)
+  const {cartData, addToCart, setCartData, removeFromCart, setCounter, counter, increase, decrease} = useContext(cartContext)
+
+  const [price, setPrice] = useState(0);
+
+  const handleRemove = (isbn) => {
+    const arr = cartData.filter((book) => book.isbn !== isbn);
+    setCartData(arr);
+    handlePrice();
+  };
+
+  const handlePrice = () => {
+    let ans = 0;
+    cartData.map((book) => (ans += book.price));
+    setPrice(ans);
+  };
+  const handleChange = (book, d) => {
+    const ind = cartData.indexOf(book);
+    const arr = cartData;
+    const result = {key: '{book.key}', title: '{book.title}', authors: '{book.author}', isbn: '{book.isbn}', cover: '{book.cover}', amount: "{d}"};
+    const newArr = [...arr, {...result}]
+    arr = newArr;
+    arr[ind].amount += d;
+
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCartData([...arr]);
+  };
+  const onAdd = (book) => {
+    const exist = cartData.find((x) => x.isbn === book.id);
+    if (exist) {
+      setCartData(
+        cartData.map((x) =>
+          x.isbn === book.isbn ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartData([...cartData, { ...book, qty: 1 }]);
+    }
+  };
+  const onRemove = (book) => {
+    const exist = cartData.find((x) => x.isbn === book.id);
+    if (exist.qty === 1) {
+      setCartData(cartData.filter((x) => x.isbn !== book.isbn));
+    } else {
+      setCartData(
+        cartData.map((x) =>
+          x.isbn === book.isbn ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+
+  useEffect(() => {
+    handlePrice();
+  });
 
     return (
       
         <article>
+        {cartData.map((book) => (   
         <div className="cart_box" >
           <div className="cart_img">
-            <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/3e213839-db48-4510-91c0-2e05eca3c6bd/air-force-1-07-lv8-mens-shoes-1BSQGs.png" alt="" />
-            <p>Nike Air Force 1</p>
+            <img src={book.cover} alt="" />
+            <p>{book.title}</p>
           </div>
           <div>
 
-            <Button variant="outlined">-</Button>
-            <Button variant="contained">3</Button>
-            <Button variant="outlined">+</Button>
+        
 
             {console.log(cartData)}
-            <button >+</button>
-            <button>3</button>
-            <button onClick={()=> removeFromCart(cartData[2]) }>-</button>
+            <Button variant = "outlined" onClick={() => onRemove(book)} >-</Button>
+            <Button variant = "contained">{counter}</Button>
+            <Button variant = "outlined" onClick={() => onAdd(book)}>+</Button>
 
           </div>
           <div>
-           <Button margin="2px" variant="text" color="success">$100</Button>
-            <Button variant="outlined" color="error">Remove</Button>
+           <Button margin="2px" variant="text" color="success">${book.price}</Button>
+            <Button onClick={() => handleRemove(book.isbn)} variant="outlined" color="error">Remove</Button>
           </div>
         </div>
+        ))}
         <div className="total">
-        <span>Total Price of your Cart</span>
-        <span>$ - $300</span>
+        <p>Total Price of your Cart</p>
+        <p>${price.toFixed(2)}</p>
       </div>
         </article>
         
