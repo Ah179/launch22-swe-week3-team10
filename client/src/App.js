ae77d8166f6d14d567de1c0daf3b59d8d0f11914
Keshav Bhateja <keshavbhateja1@gmail.com>
2022-06-10 10:43:12 -0400

    updated cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,68 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
-import Cart from "./components/Cart"
-import { useState } from 'react';
-function App() {
-  const [show, setShow] = useState(true);
-  const [cart, setCart] = useState([]);
+import {useState, useEffect} from 'react'
+import axios from "axios";
+import {Helmet} from "react-helmet"
 
-  const handleClick = (item) => {
-    if (cart.indexOf(item) !== -1) return;
-    setCart([...cart, item]);
+export const productDataContext = React.createContext()
+export const cartContext = React.createContext()
+
+function App() {
+  
+  const[productData, setProductData] = useState([])
+  const[cartData, setCartData] = useState([])
+
+  const [counter, setCounter] = useState(1);
+ 
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  //decrease counter
+  const decrease = () => {
+    setCounter(count => count - 1);
   };
 
-  const handleChange = (item, d) => {
-    const ind = cart.indexOf(item);
-    const arr = cart;
-    arr[ind].amount += d;
+  const addToCart = (data) => {
+    setCartData([...cartData, {...data, quantity: 1}])
+  }
+  
+
+  const handleChange = (book, d) => {
+    const ind = cartData.indexOf(book);
+    const arr = cartData;
+    arr[ind].quantity += d;
+
+    if (arr[ind].quantity === 0) arr[ind].quantity = 1;
+    setCartData([...arr]);
+  };
+  const removeFromCart = (data) => {
+    let i = cartData.indexOf(data)
+    const newCart = cartData;
+    newCart.splice(i, 1)
+    setCartData(newCart)
+    console.log(newCart)
+  }
+  // const handleChange = (data, d) => {
+  //   const ind = cartData.indexOf(data);
+  //   const arr = cartData;
+  //   arr[ind].amount += d;
 
     if (arr[ind].amount === 0) arr[ind].amount = 1;
     setCart([...arr]);
   };
   return (
-  
+    <productDataContext.Provider value={productData}>
+      <cartContext.Provider value={{cartData, setCartData, addToCart, removeFromCart, setCounter, counter, increase, decrease, handleChange}}>
     <div className="App">
     	<Navbar />
       <Outlet />
       <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
     </div>
+    </cartContext.Provider>
+    </productDataContext.Provider>
   );
 }
 
--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,27 +1,62 @@
 import React from 'react';
 import "./Cart.css";
-import { useState, useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import {cartContext} from '../App';
+import { Button} from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-    const Cart = ({ cart, setCart, handleChange }) => {
-    const [price, setPrice] = useState(0);
+
+function Cart() {
+    
+  const {cartData, addToCart, setCartData, removeFromCart, setCounter, counter, increase, decrease, handleChange} = useContext(cartContext)
+
+  const [price, setPrice] = useState(0);
+
+ 
+
+  const handleRemove = (isbn) => {
+    const arr = cartData.filter((book) => book.isbn !== isbn);
+    setCartData(arr);
+    handlePrice();
+  };
+
+  const handlePrice = () => {
+    let ans = 0;
+    cartData.map((book) => (ans += book.quantity*book.price));
+    setPrice(ans);
+  };
+
   
-    // const handleRemove = () => {
-    //   const arr = cart.filter((item) => item.id !== id);
-    //   setCart(arr);
-    //   handlePrice();
-    // };
-  
-    const handlePrice = () => {
-      let ans = 0;
-      cart.map((item) => (ans += item.amount * item.price));
-      setPrice(ans);
-    };
-  
-    useEffect(() => {
+  const onAdd = (book) => {
+    const exist = cartData.find((x) => x.isbn === book.isbn);
+    if (exist.quantity === 1) {
+      setCartData(
+        cartData.map((x) =>
+          x.isbn === book.isbn ? { ...exist, qty: exist.quantity + 1 } : x
+        )
+      );
       handlePrice();
-    }, []); 
-  
-    console.log(cart);
+    } else {
+      setCartData([...cartData, { ...book, quanity: 1 }]);
+      handlePrice();
+    }
+  };
+  const onRemove = (book) => {
+    const exist = cartData.find((x) => x.isbn === book.isbn);
+    if (exist) {
+      setCartData(cartData.filter((x) => x.isbn !== book.isbn));
+    } else {
+      setCartData(
+        cartData.map((x) =>
+          x.isbn === book.isbn ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+
+  useEffect(() => {
+    handlePrice();
+  });
 
     return (
         <article>
@@ -32,20 +67,27 @@ import { useState, useEffect } from 'react';
             <p>{item.title}</p>
           </div>
           <div>
-            <button onClick={() => handleChange(item, 1)}>+</button>
-            <button>{item.amount}</button>
-            <button onClick={() => handleChange(item, -1)}>-</button>
+
+        
+
+            {console.log(cartData)}
+            <Button style={{backgroundColor:'#b2dfdb', color:"#00241B"}} variant = "outlined" onClick={() => handleChange(book, -1)} >-</Button>
+            <Button  style={{color:"#00241B"}}variant = "outlined">{book.quantity}</Button>
+            <Button style={{backgroundColor:'#b2dfdb', color:"#00241B"}} variant = "outlined" onClick={() => handleChange(book, 1)}>+</Button>
+
           </div>
           <div>
-            <span>{item.price}</span>
-            {/* <button onClick={() => handleRemove(item.id)}>Remove</button> */}
+           <Button style={{ fontWeight: 'bold' , fontSize: '18px', marginRight:"80px", color:"#354259"}} margin="2px" variant="text" >${(book.price*book.quantity).toFixed(2)}</Button>
+            <Button onClick={() => handleRemove(book.isbn)} variant="outlined" color="error">Remove</Button>
           </div>
         </div>
-      ))}
-      <div className="total">
-        <span>Total Price of your Cart</span>
-        <span>$ - {price}</span>
+        ))}
+        <div className="total">
+        <Button style={{marginTop:"20px", color:"#00241B"}}>Total Price of your Cart</Button>
+        <Button style={{ marginTop:"20px" ,fontWeight: 'bold' , fontSize: '18px', marginRight:"170px", color:"#00241B"}}>${price.toFixed(2)}</Button>
+        
       </div>
+      <Button style={{ marginTop:"20px" ,fontWeight: 'bold' , fontSize: '18px', backgroundColor:'#b2dfdb', color:"#00241B"}} variant="contained" startIcon={<ShoppingCartIcon />}>Checkout</Button>
         </article>
         
     );
